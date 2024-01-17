96f27bddaf891aa8735178f0d62f805a7b51ae27
spenserg92 <spenserg92@gmail.com>
2024-01-17 14:12:01 -0500

    finished adding feedings and displaying including bonus

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
+MEALS = (
+    # this is a tuple with multiple tuples
+    # each of these is called a 2-tuple
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
 
 # Create your models here.
-
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -12,36 +20,46 @@ class Cat(models.Model):
     def __str__(self):
         return self.name
     
-# this is the get_absolute_url method, it redirects
+    # this is the get_absolute_url method, it redirects to the detail page where appropriate
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})
+    
+    # this is how we can view related data from the main parent model
+    def fed_for_today(self):
+        # we can use django's filter, which produces a queryset for all feedings.
+        # we'll look at the array(QuerySet) and compare it to the length of the MEALS tuple
+        # we can return a boolean, that will be useful in our detail template
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
-def get_absolute_url(self):
-    return reverse('detail', kwargs={'cat_id': self.id})
 
-MEALS = (
-    # this is a tuple with multiple tuples
-    # each of these is called a 2-tuple
-    ('B', 'Breakfast'),
-    ('L', 'Lunch'),
-    ('D', 'Dinner')
-)
-
-# The model for feedings 1:M
-# One cat can have many feedings
+# The model for feedings - this is a 1:M relationship with cats
+    # one cat can have many feedings
 class Feeding(models.Model):
-    date = models.DateField()
+    # our model attributes go here
+    # we can add a custom label to show up on our forms
+    date = models.DateField('feeding date')
+    # meals are a charfield with max_length of one, because we're only going to save the first initial of each meal
+    # this will help generate a dropdown in the automagically created modelform
+    # B-reakfast
+    # L-unch
+    # D-inner
     meal = models.CharField(
         max_length=1,
-        #add the custom 'choices' field option
+        # add the custom 'choices' field option
         # this is what will create our dropdown menu
         choices=MEALS,
-        #set the default to B
+        # set the default choice, to be 'B'
         default=MEALS[0][0]
     )
-    # Creates the one to many relationship - Cat -< Feedings
-    # models.ForeignKey needs 2 args, the model, and what to do if the parent model is deleted
-    # in the db, the column in the feedings table for the FK will be called cat_id, because Django, by default, appends_id to the name of the model
-    # DONT CONFUSE THIS WITH MONGODB AND THEIR `._id` NOT THE SAME
+    # creates the one to many relationship - Cat -< Feedings
+    # models.ForeignKey needs two args, the model, and what to do if the parent model is deleted.
+    # in the db, the column in the feedings table for the FK will be called cat_id, because django, by default, appends _id to the name of the model
+    # DO NOT CONFUSE THIS WITH MONGODB AND THEIR `._id` NOT THE SAME
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date} for {self.cat}"
+    
+    # change the default sort
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,56 +1,84 @@
-from django.shortcuts import render
-# importing our Class-based-views (CBV's)
+from django.shortcuts import render, redirect
+# importing our Class-Based-Views (CBVs)
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Cat
-## this was to build our intial view
+from .forms import FeedingForm
+# Add this cats list below the imports
+# this was to build our inital view - now we have cats in the db
 # cats = [
-#     {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-#     {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#   {'name': 'Donut', 'breed': 'siamese', 'description': 'cute but kinda scary', 'age': 0},
 # ]
+
 # Create your views here.
 
-# Define home view here - '/'
-
+# define home view here - '/'
+# GET - Home
 def home(request):
-    #unlike with EJS, we need our .html file extension
-    return render(request, "home.html")
+    # unlike with ejs, we need our .html file extension
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-# index view - shows all the cats at /cats
-
+# index view - shows all the cats at '/cats'
 def cats_index(request):
-    # collect objects
+    # collect our objects from the database
+    # this uses the objects object on the Cat model class
+    # the objects object has a method called all
+    # all grabs all of the entities using the parent model(in this case, Cat)
     cats = Cat.objects.all()
-
+    # print(cats)
     # for cat in cats:
     #     print(cat)
-    return render(request, 'cats/index.html', {'cats': cats})
+    # just like in ejs, we can pass some data to our views
+    return render(request, 'cats/index.html', { 'cats': cats })
 
-## detail view
-
+# detail view - shows one cat at '/cats/:id'
 def cats_detail(request, cat_id):
+    # find one cat with its id
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat' : cat})
+    # instantiate FeedingForm to be rendered in our template
+    feeding_form = FeedingForm()
 
-# Inherit from the CBV - CreateView
+    return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form })
 
+# inherit from the CBV - CreateView, to make our cats create view
 class CatCreate(CreateView):
+    # tell the createview to use the Cat model for all its functionality
     model = Cat
-    # this view creates a form
+    # this view creates a form, so we need to identify which fields to use
     fields = '__all__'
-    # We can add other options inside this view
-    success_url = '/cats/'
+    # we can add other options inside this view
+    # success_url = '/cats/{cat_id}'
 
-# Update View
-
+# Update View - extends the UpdateView class
 class CatUpdate(UpdateView):
     model = Cat
-    fields = {'breed', 'description', 'age'}
+    # let's make it so you can't rename a cat
+    # we could simply say fields = '__all__', or we can customize like this:
+    fields = ['breed', 'description', 'age']
 
-# Delete - View
-
+# Delete View - extends DeleteView
 class CatDelete(DeleteView):
     model = Cat
-    success_url = "/cats"
+
+    success_url = '/cats'
+
+# FEEDING AND RELATIONSHIP VIEW FUNCTIONS
+# this is to add a feeding to a cat
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # it's also important to validate forms.
+    # django gives us a built in function for that
+    if form.is_valid():
+        # dont want to save the feeding to the db until we have a cat id
+        new_feeding = form.save(commit=False)
+        # this is where we add the cat id
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    # finally, redirect to the cat detail page
+    return redirect('detail', cat_id=cat_id)
